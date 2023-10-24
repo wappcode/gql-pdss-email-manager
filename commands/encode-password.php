11070de9a049f1c3e28b7fad4b560dbf13463c54
Jesus Abarca <jesus.abarca.g@gmail.com>
2023-10-24 12:24:59 -0600

    Se agrego comando para codificar las contraseñas. Este comando no debe ser publico Se actualizo ejemplo del comando para enviar los correos usando por ejemplo cron

--- commands/encode-password.php
+++ commands/encode-password.php
@@ -4,9 +4,10 @@ use GPDCore\Library\GPDApp;
 use AppModule\Services\AppRouter;
 use GPDCore\Services\ContextService;
 use Laminas\ServiceManager\ServiceManager;
-use GPDEmailManager\Services\EmailProcessQueue;
+use GPDEmailManager\Library\EmialPassworEncoder;
 
 require_once __DIR__ . "/../vendor/autoload.php";
+
 date_default_timezone_set('America/Mexico_City');
 
 $enviroment = GPDApp::ENVIROMENT_DEVELOPMENT; // Cambiar a producción al publicarlo
@@ -20,8 +21,12 @@ $context->setDoctrineCacheDir($entityCache);
 $router = new AppRouter();
 $app = new GPDApp($context, $router, $enviroment);
 
-
 $localConfig = require __DIR__ . "/../dev/config/local.config.php";
 $context->getConfig()->add($localConfig);
-
-EmailProcessQueue::proccessAll($context);
+$encriptPassword = $context->getConfig()->get("gpd_email_manager__secret_key");
+$iv = $context->getConfig()->get("gql_email_manager__iv");
+$password = readline("\nEnter a password to encode: \n");
+$encodedPassword = EmialPassworEncoder::encrypt($password, $encriptPassword, $iv);
+$decodePassword = EmialPassworEncoder::decrypt($encodedPassword, $encriptPassword, $iv);
+echo "\nThe encoded password is {$encodedPassword}\n";
+echo "\nThe decoded password is {$decodePassword}\n";
