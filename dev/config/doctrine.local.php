23f51fb763d437bfee7319ccf908c2c6f734c4fb
J Jesus Abarca <jesus.abarca.g@gmail.com>
2024-06-11 14:25:23 -0600

    Actualización configuración doctrine

--- dev/config/doctrine.local.php
+++ dev/config/doctrine.local.php
@@ -1,11 +1,11 @@
 <?php
 return [
     "driver" => [
-        'user'     =>   'root',
-        'password' =>   'dbpassword',
-        'dbname'   =>   'gqlpdss_emaildb',
+        'user'     =>   getenv("GQLPDSSEMAIL_DBPASSWORD") ? getenv("GQLPDSSEMAIL_DBUSER") : 'root',
+        'password' =>   getenv("GQLPDSSEMAIL_DBPASSWORD") ? getenv("GQLPDSSEMAIL_DBPASSWORD") : 'dbpassword',
+        'dbname'   =>   getenv("GQLPDSSEMAIL_DBNAME") ? getenv("GQLPDSSEMAIL_DBNAME") : 'gqlpdss_emaildb',
         'driver'   =>   'pdo_mysql',
-        'host'   =>     'gqlpdsemail-mysql',
+        'host'   =>     getenv("GQLPDSSEMAIL_DBHOST") ? getenv("GQLPDSSEMAIL_DBHOST") : 'gqlpdsemail-mysql',
         'charset' =>    'utf8mb4'
     ],
     "entities" => require __DIR__ . "/doctrine.entities.php"
